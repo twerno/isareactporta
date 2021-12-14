18d1b16dce1ea87dcdb444ce15bff7eb1b10adb4
Kamil Richert <krichert@atlassian.com>
2021-12-14 11:31:07 +0100

    Added button with on click fot Contact

--- day1_2/my-app/src/Contact.js
+++ day1_2/my-app/src/Contact.js
@@ -3,6 +3,10 @@ import './Contact.css';
 export function Contact(props) {
     const { data: { phone, address, email } } = props;
 
+    const sendData = () => {
+        alert(`Dziękuję! Zapraszam do mnie przy ${address.street} ${address.number} ${address.city}!`)
+    }
+
     return <div className="contact-container">
         <h3 className="contact-heading">TEL</h3>
         <p>{phone}</p>
@@ -10,5 +14,6 @@ export function Contact(props) {
         <p>{address.street} {address.number} {address.city}</p>
         <h3 className="contact-heading">EMAIL</h3>
         <p>{email}</p>
+        <button onClick={sendData}>Wyślij</button>
     </div>
 }
