a2d168483afbdbbb64c58933302353a13fc985fd
Kamil Richert <krichert@atlassian.com>
2021-12-14 14:29:40 +0100

    Added Users component with simple fetching data

--- day1_2/my-app/src/App.js
+++ day1_2/my-app/src/App.js
@@ -1,12 +1,14 @@
 import { Wrapper } from './Wrapper';
 import { AboutMe } from './about-me/AboutMe';
 import { Game } from './Game';
+import { Users } from './Users';
 
 function App() {
     return (
         <Wrapper>
             <AboutMe />
             <Game name="Witcher 3" />
+            <Users />
         </Wrapper>
     );
 }
---
+++ day1_2/my-app/src/Users.js
@@ -0,0 +1,21 @@
+import {useState, useEffect} from 'react';
+
+export const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(r => r.json())
+            .then(data => {
+                setUsers(data);
+            })
+    }, []);
+
+    return <ul>
+        {users.map(user => {
+            return <li key={user.id}>
+                {user.name} works in {user.company.name}
+            </li>
+        })}
+    </ul>
+}
