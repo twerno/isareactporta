f8046cb7d95e1ac945c4a1c3e4e50b362b0f773e
Kamil Richert <krichert@atlassian.com>
2021-12-14 13:53:03 +0100

    Added chaning state in Game component and displaying conditional styles

--- day1_2/my-app/src/Game.js
+++ day1_2/my-app/src/Game.js
@@ -1,10 +1,25 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
-export const Game = ({ name }) => {
+export const Game = ({name}) => {
     const [points, setPoints] = useState(0);
+
+    const increase = () => {
+        setPoints(points + 5);
+    }
+
+    const decrease = () => {
+        setPoints(points - 5);
+    }
 
     return <>
         <h3>Witam w grze {name}!</h3>
-        <h4>Twoje punkty to: {points}</h4>
+        <h4>
+            Twoje punkty to:
+            <span style={{ color: points < 0 ? 'red' : 'inherit'}}> {points}</span>
+        </h4>
+        <div>
+            <button onClick={increase}>+</button>
+            <button onClick={decrease}>-</button>
+        </div>
     </>
 }
