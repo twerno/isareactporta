2c0b8b5a5a570d1da38390b1702fbbc86fd5e5c5
Kamil Richert <krichert@atlassian.com>
2021-12-14 10:34:58 +0100

    Removed unused code

--- day1_2/my-app/src/App.js
+++ day1_2/my-app/src/App.js
@@ -1,24 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Wow ale React jest fajny!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Cos tam</h1>
   );
 }
 
