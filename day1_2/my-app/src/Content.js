45addada3c670d84c91f47978c8264a34728897e
Kamil Richert <krichert@atlassian.com>
2021-12-15 15:05:28 +0100

    Added lazy loading for about me compoonent

--- day1_2/my-app/src/Content.js
+++ day1_2/my-app/src/Content.js
@@ -1,39 +1,45 @@
+import {lazy, Suspense} from 'react';
 import {Route, Switch} from "react-router-dom";
-import {AboutMe} from "./about-me/AboutMe";
 import {Game, GameClass} from "./games";
 import {BetterForm, BoostedForm, Form} from "./forms";
 import {Users} from "./users/Users";
 import {UserDetails} from "./users/UserDetails";
 import {Sign} from "./sign/Sign";
 import {Wrapper} from "./Wrapper";
+import CircularProgress from '@mui/material/CircularProgress';
+
+const AboutMeLazy = lazy(() => import("./about-me/AboutMe")
+    .then((module) => ({default: module.AboutMe})));
 
 export const Content = () => {
     return <Wrapper>
-        <Switch>
-            <Route exact path="/">
-                <h1>Witaj na naszej stronie!</h1>
-            </Route>
-            <Route path="/about-me">
-                <AboutMe/>
-            </Route>
-            <Route path="/games">
-                <Game name="Witcher 3"/>
-                <GameClass name="Diablo 2"/>
-            </Route>
-            <Route path="/forms">
-                <Form/>
-                <BetterForm/>
-                <BoostedForm/>
-            </Route>
-            <Route exact path="/users">
-                <Users/>
-            </Route>
-            <Route path="/users/:id">
-                <UserDetails/>
-            </Route>
-            <Route path="/sign">
-                <Sign/>
-            </Route>
-        </Switch>
+        <Suspense fallback={<CircularProgress/>}>
+            <Switch>
+                <Route exact path="/">
+                    <h1>Witaj na naszej stronie!</h1>
+                </Route>
+                <Route path="/about-me">
+                    <AboutMeLazy/>
+                </Route>
+                <Route path="/games">
+                    <Game name="Witcher 3"/>
+                    <GameClass name="Diablo 2"/>
+                </Route>
+                <Route path="/forms">
+                    <Form/>
+                    <BetterForm/>
+                    <BoostedForm/>
+                </Route>
+                <Route exact path="/users">
+                    <Users/>
+                </Route>
+                <Route path="/users/:id">
+                    <UserDetails/>
+                </Route>
+                <Route path="/sign">
+                    <Sign/>
+                </Route>
+            </Switch>
+        </Suspense>
     </Wrapper>
 }
