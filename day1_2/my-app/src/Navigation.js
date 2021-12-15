3c82f9b2eddd31d6d8a1499641aa6b14a66de980
Kamil Richert <krichert@atlassian.com>
2021-12-15 11:57:46 +0100

    Changed Link to Navlink in navigation component to add active class

--- day1_2/my-app/src/Navigation.js
+++ day1_2/my-app/src/Navigation.js
@@ -5,11 +5,12 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import Icon from '@mui/material/Icon';
-import { Link } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
+import './Navigation.css';
 
 export default function Navigation() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{flexGrow: 1}}>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton
@@ -17,15 +18,20 @@ export default function Navigation() {
                         edge="start"
                         color="inherit"
                         aria-label="menu"
-                        sx={{ mr: 2 }}
+                        sx={{mr: 2}}
                     >
                         <Icon>favorite</Icon>
                     </IconButton>
-                    <Button color="inherit" component={Link} to="/">Home</Button>
-                    <Button color="inherit" component={Link} to="/about-me">About me</Button>
-                    <Button color="inherit" component={Link} to="/games">Games</Button>
-                    <Button color="inherit" component={Link} to="/forms">Forms</Button>
-                    <Button color="inherit" component={Link} to="/users">Users</Button>
+                    <Button color="inherit" component={NavLink} activeClassName="active-nav-link" exact
+                            to="/">Home</Button>
+                    <Button color="inherit" component={NavLink} activeClassName="active-nav-link" to="/about-me">About
+                        me</Button>
+                    <Button color="inherit" component={NavLink} activeClassName="active-nav-link"
+                            to="/games">Games</Button>
+                    <Button color="inherit" component={NavLink} activeClassName="active-nav-link"
+                            to="/forms">Forms</Button>
+                    <Button color="inherit" component={NavLink} activeClassName="active-nav-link"
+                            to="/users">Users</Button>
                 </Toolbar>
             </AppBar>
         </Box>
