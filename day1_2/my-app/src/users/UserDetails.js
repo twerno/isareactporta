d600b80ce8a8457bc19c0485109846440d5618a5
Kamil Richert <krichert@atlassian.com>
2021-12-15 13:48:57 +0100

    Added fetching data in UserDetails

--- day1_2/my-app/src/users/UserDetails.js
+++ day1_2/my-app/src/users/UserDetails.js
@@ -1,7 +1,24 @@
+import {useState, useEffect} from 'react';
+import CircularProgress from '@mui/material/CircularProgress'
 import {useParams} from 'react-router-dom';
 
 export const UserDetails = () => {
+    const [userData, setUserData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
     const params = useParams();
 
-    return <h1>Witaj użytkowniku o id: {params.id}</h1>
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+            .then(r => r.json())
+            .then(data => {
+                setUserData(data);
+                setIsLoading(false);
+            })
+    }, []);
+
+    if (isLoading && !userData) {
+        return <CircularProgress />
+    }
+
+    return <h1>Witaj {userData.name}!</h1>
 }
