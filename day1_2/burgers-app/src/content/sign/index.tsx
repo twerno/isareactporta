a2685dda3c3f56dca4142c4202d3603f8749944d
Kamil Richert <krichert@atlassian.com>
2021-12-16 13:13:49 +0100

    Installed firebase and create sign component

--- day1_2/burgers-app/src/content/sign/index.tsx
+++ day1_2/burgers-app/src/content/sign/index.tsx
@@ -1,7 +1,49 @@
+import {ChangeEvent, useState} from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import styled from "styled-components";
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import {PageWrapper} from "../../common/page-wrapper";
 
+interface SignFormState {
+    email: string;
+    password: string;
+}
+
+const FormContainer = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
 export const Sign = () => {
+    const [formData, setFormData] = useState<SignFormState>({
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, formData.email, formData.password);
+    }
+
+    const {email, password} = formData;
+
     return <PageWrapper title="Sign">
-        <h1>Hello</h1>
+        <FormContainer onSubmit={handleSubmit}>
+                <TextField sx={{marginBottom: '10px'}} label="Email" type="email" name="email" value={email}
+                           onChange={handleChange}/>
+                <TextField sx={{marginBottom: '10px'}} label="Password" type="password" name="password" value={password}
+                           onChange={handleChange}/>
+                <Button type="submit">Log in</Button>
+            </FormContainer>
     </PageWrapper>
 }
--- day1_2/burgers-app/src/index.tsx
+++ day1_2/burgers-app/src/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Tylko na potrzeby logowania
+const firebaseConfig = {
+    apiKey: "AIzaSyBJR7fEfWgZSUM7m-BTeud5iNlibRwuvow",
+    authDomain: "rest-api-b6410.firebaseapp.com",
+    databaseURL: "https://rest-api-b6410.firebaseio.com",
+    projectId: "rest-api-b6410",
+    storageBucket: "rest-api-b6410.appspot.com",
+    messagingSenderId: "278242273749",
+    appId: "1:278242273749:web:f47f0beae70d68a555b55c"
+};
+
+initializeApp(firebaseConfig);
+// ---------------------------
 
 ReactDOM.render(
     <React.StrictMode>
