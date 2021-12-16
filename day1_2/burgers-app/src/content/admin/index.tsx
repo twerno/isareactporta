1103537462b79f2f62c0243de8e5044c567548e8
Kamil Richert <krichert@atlassian.com>
2021-12-16 10:32:09 +0100

    Added table to admin page

--- day1_2/burgers-app/src/content/admin/index.tsx
+++ day1_2/burgers-app/src/content/admin/index.tsx
@@ -1,7 +1,45 @@
 import {PageWrapper} from "../../common/page-wrapper";
+import {useEffect, useState} from "react";
+import {Burger} from "../../common/types";
+import {fetchBurgers} from "../../services/fetch-burgers";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
 
 export const Admin = () => {
-    return <PageWrapper title="Admin">
-        <h1>Hello</h1>
-    </PageWrapper>
+    const [burgers, setBurgers] = useState<Burger[] | null>(null);
+
+    useEffect(() => {
+        fetchBurgers()
+            .then(data => {
+                setBurgers(data);
+            })
+    }, []);
+
+    return (
+        <PageWrapper title="Admin">
+            <Table sx={{maxWidth: '75%'}}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Ingredients</TableCell>
+                        <TableCell align="right">Price</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {burgers?.map(burger => {
+                        return (
+                            <TableRow key={burger.id}>
+                                <TableCell>{burger.name}</TableCell>
+                                <TableCell align="right">{burger.ingredients}</TableCell>
+                                <TableCell align="right">{burger.price}</TableCell>
+                            </TableRow>
+                        )
+                    })}
+                </TableBody>
+            </Table>
+        </PageWrapper>
+    )
 }
